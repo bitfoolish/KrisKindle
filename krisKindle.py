d984138333a5c50ea1c69ce0adbd8baa2e97da91
Shane Traynor <shanetraynor21+GH@gmail.com>
2025-07-08 23:42:03 +0100

    error messages for incorrect input types + email sent notification added

--- krisKindle.py
+++ krisKindle.py
@@ -20,16 +20,25 @@ participant = input()
 while participant != "end": # take in User's name + email
     TOKENS = participant.split()
     numTokens = len(TOKENS) 
+    partEmail = TOKENS[numTokens-1].lower()
     jointName = " ".join(TOKENS[:numTokens-1])
-    if (names.get(jointName.capitalize()) == None) : # if that name doesnt already exist, puts each inputted name in standardised form: joHn => John
-        names[jointName.capitalize()] = TOKENS[numTokens-1]
+
+    if ( (names.get(jointName.capitalize()) == None) and (partEmail not in names.values()) ) : # if that name doesnt already exist, puts each inputted name in standardised form: joHn => John
+        names[jointName.capitalize()] = partEmail
         #print(names)
-    else: # name already added to the pot
+    
+    elif ( (names.get(jointName.capitalize()) != None) and ((partEmail not in names.values()))): # Non-Unique name, Unique email
+        print("name??")
         print(f"That name {jointName.capitalize()} is already entered! Please enter a unique name")
     
-    participant = input()
+    elif ( (partEmail in names.values()) and (names.get(jointName.capitalize()) == None)): # Non-Unique email, Unique name
+        print("email??")
+        print(f"That email {partEmail} is already entered! Please enter a unique email of your own")
+    
+    else: # Non-Unique name and Non-unique email
+        print(f"{jointName, partEmail} have already been added")
 
-#print(names) 
+    participant = input()
 
 ogGifter = list(names.keys())# E.g. ['lisa', 'ann', 'mary'] => names of people involved in secret santa
 ogGiftee = ogGifter.copy()  # ['lisa', 'ann', 'mary']
@@ -57,9 +66,9 @@ while(x < gifts):
         x = 0
         gifter = ogGifter.copy()
         giftee = ogGiftee.copy()
-        print("mistakes were made, we go again")
-        print(names)
-        print(names[buy], names[rcvr])
+        # print("mistakes were made, we go again")
+        # print(names)
+        # print(names[buy], names[rcvr])
 
 
 msg = EmailMessage()
@@ -69,11 +78,11 @@ msg['From'] = senderEmail
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server: # 465 is the port number for SSL
     server.login(senderEmail, senderPassword)
-    
     for gifter, giftee in pairs.items():
         rcvrEmail = names[gifter]
         msg['To'] = rcvrEmail
         msg.set_content(f"You {gifter} got {giftee} !!")
         server.send_message(msg) 
         del msg['To']
+        print(f"Email sent to {rcvrEmail}!")
             
